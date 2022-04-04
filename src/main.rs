5b82628058543f1baedd4890135d9af708cd5800
Jonathan Lange <jml@mumak.net>
2022-04-04 08:00:05 +0100

    Clarify example

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,11 @@ use tokio;
 async fn main() -> io::Result<()> {
     let xs = vec![Ok(1), Ok(2)];
     let xs_iter = iter(xs);
-    let progress = ProgressBar::new(4);
+    // TODO: It is hilarious how much code it is to pass the length of the vector to the progress bar.
+    // I am too tired to figure it out right now.
+    let progress = ProgressBar::new(2);
+    // TODO: This code does not compile.
+    // error[E0507]: cannot move out of `progress`, a captured variable in an `FnMut` closure
     xs_iter.try_for_each_concurrent(1, |x| async move {
         println!("{}", x);
         progress.inc(1);
