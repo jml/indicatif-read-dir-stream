75645c66497e7c2883a04240ccb8162e6d10f3ef
Jonathan Lange <jml@mumak.net>
2022-04-04 16:43:52 +0100

    Make a wrapper iterator

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use futures::stream::{TryStreamExt, iter};
+use core::pin::Pin;
+use futures::stream::{self, TryStreamExt, iter};
+use futures::task::{Context, Poll};
 use indicatif::ProgressBar;
 use std::io;
 use tokio;
@@ -11,15 +13,36 @@ async fn main() -> io::Result<()> {
 }
 
 
+struct ProgressStream<S: stream::Stream> {
+    progress_bar: ProgressBar,
+    stream: S,
+}
+
+
+impl<S: stream::Stream + std::marker::Unpin> stream::Stream for ProgressStream<S> {
+    type Item = S::Item;
+
+    fn poll_next(mut self: Pin<&mut Self>, ctx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        match Pin::new(&mut self.stream).poll_next(ctx) {
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(None) => Poll::Ready(None),
+            Poll::Ready(Some(x)) => {
+                self.progress_bar.inc(1);
+                Poll::Ready(Some(x))
+            }
+        }
+    }
+}
+
+
 async fn async_loop() -> io::Result<()> {
     let xs = vec![Ok(1), Ok(2)];
     let progress = ProgressBar::new(xs.len() as u64);
-    let xs_iter = iter(xs);
+    let progress_stream = ProgressStream { progress_bar: progress, stream: iter(xs) };
     // TODO: This code does not compile.
     // error[E0507]: cannot move out of `progress`, a captured variable in an `FnMut` closure
-    xs_iter.try_for_each(|x| async move {
+    progress_stream.try_for_each(|x| async move {
         println!("{}", x);
-        progress.inc(1);
         Ok(())
     }).await
 }
