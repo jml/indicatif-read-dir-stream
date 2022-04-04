469d8ab663ca8fb450e395124223c49afe90b01c
Jonathan Lange <jml@mumak.net>
2022-04-04 07:55:40 +0100

    Simplify example by removing file I/O
    Still need ProgressBar explicitly.
    Still not sure what's going on.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,17 @@
-use futures::stream::TryStreamExt;
+use futures::stream::{TryStreamExt, iter};
 use indicatif::ProgressBar;
-use std::convert::TryInto;
 use std::io;
-use std::path::Path;
-use std::fs as std_fs;
-use tokio::fs;
-use tokio_stream::wrappers::ReadDirStream;
+use tokio;
 
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let source_dir = Path::new("/");
-    let num_files = std_fs::read_dir(&source_dir)?.count();
-    let progress = ProgressBar::new(num_files.try_into().unwrap());
-    let source_dir = fs::read_dir(&source_dir).await?;
-    let stream = ReadDirStream::new(source_dir);
-    let num_processes = 4;
-    stream
-        .try_for_each_concurrent(num_processes, |_source_file| async move {
-            progress.inc(1);
-            Ok(())
-        }).await?;
-    Ok(())
+    let xs = vec![Ok(1), Ok(2)];
+    let xs_iter = iter(xs);
+    let progress = ProgressBar::new(4);
+    xs_iter.try_for_each_concurrent(1, |x| async move {
+        println!("{}", x);
+        progress.inc(1);
+        Ok(())
+    }).await
 }
