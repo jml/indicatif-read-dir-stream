0ccfb62b1986cdd2dc52fe1d1c4e046fbf80efa9
Jonathan Lange <jml@mumak.net>
2022-04-04 09:13:09 +0100

    Synchronous example plus len() fix

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,15 @@ use tokio;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    sync_loop();
+    async_loop().await
+}
+
+
+async fn async_loop() -> io::Result<()> {
     let xs = vec![Ok(1), Ok(2)];
+    let progress = ProgressBar::new(xs.len() as u64);
     let xs_iter = iter(xs);
-    // TODO: It is hilarious how much code it is to pass the length of the vector to the progress bar.
-    // I am too tired to figure it out right now.
-    let progress = ProgressBar::new(2);
     // TODO: This code does not compile.
     // error[E0507]: cannot move out of `progress`, a captured variable in an `FnMut` closure
     xs_iter.try_for_each(|x| async move {
@@ -18,4 +22,15 @@ async fn main() -> io::Result<()> {
         progress.inc(1);
         Ok(())
     }).await
+}
+
+
+fn sync_loop() {
+    let xs = vec![1, 2];
+    let progress = ProgressBar::new(xs.len() as u64);
+    let xs_iter = xs.into_iter();
+    xs_iter.for_each(|x| {
+        println!("{}", x);
+        progress.inc(1);
+    });
 }
